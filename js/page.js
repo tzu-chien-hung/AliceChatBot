79d99c578f2753f9149f70c037f8fd7cf94283cc
tzu-chien-hung <tina19960522@gmail.com>
2022-04-26 09:52:19 +0800

    Tina | update fb page json

--- js/page.js
+++ js/page.js
@@ -1,51 +1,11 @@
 const ul = document.getElementById('case_slider_all_container');
 const li = document.querySelectorAll('.slider_big_img');
 const fb_message = document.getElementById('fb-root');
-console.log(fb_message);
+// console.log(fb_message);
 let index = 0;
 let left_index = 1;
 let arrow_left = document.getElementById('slider_arrow_left');
 let arrow_right = document.getElementById('slider_arrow_right');
-
-// index++;
-// ul.style.transform = `translateX(${(-index * 50)}%)`;
-// if ((index) % (li.length) == 0) {
-//     ul.style.left = left_index * 500 + "%";
-//     left_index++;
-// }
-arrow_left.addEventListener('click', function () {
-    index--;
-    ul.style.transform = `translateX(${(-index * 50)}%)`;
-    if ((index - 4) % (li.length) == 0) {
-        ul.style.left = (left_index - 2) * 500 + "%";
-        left_index--;
-    };
-    setTimeout(function () {
-      fb_message.style.display= 'block';
-    }, 1000);
-});
-// console.log(fb_message);
-arrow_right.addEventListener('click', function () {
-    index++;
-    ul.style.transform = `translateX(${(-index * 50)}%)`;
-    if ((index) % (li.length) == 0) {
-        ul.style.left = left_index * 500 + "%";
-        left_index++;
-    };
-    fb_message.style.display= 'none';
-});
-// ==========================  loging 頁面的js =============================
-const yt_loding = document.getElementById('yt_loding');
-const fb_loding = document.getElementById('fb_loding');
-const fb_loding_close_btn = document.getElementById('fb_loding_close_btn');
-const yt_loding_close_btn = document.getElementById('yt_loding_close_btn');
-fb_loding_close_btn.addEventListener('click', function () {
-  fb_loding.style.display= 'none';
-});
-yt_loding_close_btn.addEventListener('click', function () {
-  yt_loding.style.display= 'none';
-});
-
 
 // ==========================  yt =============================
 
@@ -59,20 +19,17 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-
+var player;
 function onYouTubeIframeAPIReady() {
-  var player;
+  
   player = new YT.Player('player', {
     height: '535.91',
     width: '900',
     videoId: 'gId9xFyID0o',
     events: {
       // 'onReady': onPlayerReady,
-      // 'onStateChange': onPlayerStateChange
     },
-    playerVars: {
-      // 'autoplay': 1, 'controls': 0,
-    },
+
   });
 
 }
@@ -96,3 +53,40 @@ function stopVideo() {
     // console.log('aa')
   player.stopVideo();
 }
+
+arrow_left.addEventListener('click', function () {
+    stopVideo();
+    index--;
+    ul.style.transform = `translateX(${(-index * 50)}%)`;
+    if ((index - 4) % (li.length) == 0) {
+        ul.style.left = (left_index - 2) * 500 + "%";
+        left_index--;
+    };
+    setTimeout(function () {
+      fb_message.style.display= 'block';
+    }, 1000);
+});
+// console.log(fb_message);
+arrow_right.addEventListener('click', function () {
+  stopVideo();
+    index++;
+    ul.style.transform = `translateX(${(-index * 50)}%)`;
+    if ((index) % (li.length) == 0) {
+        ul.style.left = left_index * 500 + "%";
+        left_index++;
+    };
+    fb_message.style.display= 'none';
+});
+// ==========================  loging 頁面的js =============================
+const yt_loding = document.getElementById('yt_loding');
+const fb_loding = document.getElementById('fb_loding');
+const fb_loding_close_btn = document.getElementById('fb_loding_close_btn');
+const yt_loding_close_btn = document.getElementById('yt_loding_close_btn');
+fb_loding_close_btn.addEventListener('click', function () {
+  fb_loding.style.display= 'none';
+});
+yt_loding_close_btn.addEventListener('click', function () {
+  yt_loding.style.display= 'none';
+});
+
+
